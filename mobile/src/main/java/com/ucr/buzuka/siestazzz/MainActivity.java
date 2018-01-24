81a652822185dea9858ca784312b282f5328125f
Rick <rwboshae@gmail.com>
2018-01-23 22:43:01 -0800

    Added 2 Action Bars at the top of the application.

--- mobile/src/main/java/com/ucr/buzuka/siestazzz/MainActivity.java
+++ mobile/src/main/java/com/ucr/buzuka/siestazzz/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ucr.buzuka.siestazzz;
 
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar(); // or getActionBar()
+        actionBar.hide();
 
         mViewPager = (ViewPager) findViewById(R.id.viewPager);
 
