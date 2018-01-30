e2e737eeed017690adb3acc8fd347c6ea288f084
Rick <rwboshae@gmail.com>
2018-01-29 19:31:26 -0800

    Added Tab Layout

--- mobile/src/main/java/com/ucr/buzuka/siestazzz/MainViewPageFragment.java
+++ mobile/src/main/java/com/ucr/buzuka/siestazzz/MainViewPageFragment.java
@@ -3,8 +3,6 @@ package com.ucr.buzuka.siestazzz;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,22 +19,22 @@ public class MainViewPageFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View mView = inflater.inflate(R.layout.main_viewpage_fragment,null); // inflate is a function that converts a layout to a view.
+        View mView = inflater.inflate(R.layout.fragment_main,null); // inflate is a function that converts a layout to a view.
 
-        return inflater.inflate(R.layout.main_viewpage_fragment,null); // inflate is a function that converts a layout to a view.
+        return inflater.inflate(R.layout.fragment_main,null); // inflate is a function that converts a layout to a view.
 
 
     }
 
-    protected void onCreate(Bundle savedInstanceState) {
-        Button button = findViewById(R.id.sleepButton);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Button Clicked",Toast.LENGTH_LONG).show();
-            }
-        });
-    }
+//    protected void onCreate(Bundle savedInstanceState) {
+//        Button button = findViewById(R.id.sleepButton);
+//
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this, "Button Clicked",Toast.LENGTH_LONG).show();
+//            }
+//        });
+//    }
 
 }
