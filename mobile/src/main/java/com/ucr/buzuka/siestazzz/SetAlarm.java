12101b51af60f2608dc39a5b86c32c9a18fb3aa7
Ryan Ulep <rulep001@ucr.edu>
2018-02-04 21:20:54 -0800

    Changed button to set alarm on intent

--- mobile/src/main/java/com/ucr/buzuka/siestazzz/SetAlarm.java
+++ mobile/src/main/java/com/ucr/buzuka/siestazzz/SetAlarm.java
@@ -12,4 +12,8 @@ public class SetAlarm extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_alarm);
     }
+
+    public void setAlarmTime(View view) {
+
+    }
 }
