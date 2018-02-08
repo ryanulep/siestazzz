6f0fc0c50bacb7857d6d97e538a10b4d4dd0bb33
Ryan Ulep <rulep001@ucr.edu>
2018-02-07 17:49:08 -0800

    Created cancel button actions

--- mobile/src/main/java/com/ucr/buzuka/siestazzz/SetAlarm.java
+++ mobile/src/main/java/com/ucr/buzuka/siestazzz/SetAlarm.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SetAlarm extends AppCompatActivity {
 
@@ -11,9 +13,14 @@ public class SetAlarm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_alarm);
-    }
 
-    public void setAlarmTime(View view) {
+        Button cancelButton = (Button) findViewById(R.id.cancelButton);
 
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
