fe13c776d80daa20a39e817b910904a21d01a2c4
xzhou016 <xzhou016@ucr.edu>
2018-02-11 21:42:08 -0800

    NEW: write to file WIP

--- mobile/src/main/java/com/ucr/buzuka/siestazzz/SleepSessionActivity.java
+++ mobile/src/main/java/com/ucr/buzuka/siestazzz/SleepSessionActivity.java
@@ -8,14 +8,21 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ucr.buzuka.siestazzz.model.SensorReadout;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class SleepSessionActivity extends AppCompatActivity implements SensorEventListener {
 
     //sensor manager and accelerometer
     private SensorManager sensorManager;
     private Sensor sensorAccelerometer;
+    private int idInc = 0;
 
     //set the time interval to pull from sensor, current 500 ms
     private static final int M_SENSOR_DELAY = 500;
@@ -24,8 +31,13 @@ public class SleepSessionActivity extends AppCompatActivity implements SensorEve
     private long lastUpdate = 0;
     private static final long M_POLL_INTERVAL = 1000; // not used, but it is for displaying sensor data on app
     private float last_x, last_y, last_z; //last position
+    private static final float SENSOR_THRESHOLD = 5.0f;
     //private Queue<Float> sensorLog;
 
+    public static final String FILE_NAME = "readout.txt";
+    private static FileOutputStream fileOutputStream = null;
+    private static File file = new File(FILE_NAME);
+    private static SensorReadout readout;
 
 
     @Override
@@ -34,17 +46,17 @@ public class SleepSessionActivity extends AppCompatActivity implements SensorEve
         setContentView(R.layout.activity_sleep_session);
 
         //new sensor read out
-        SensorReadout read =
-                new SensorReadout(null,
-                                "first",
-                                System.currentTimeMillis(),
-                                System.currentTimeMillis(),
-                                0,
-                                0,
-                                0,
-                                0);
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(read.toString());
+//        SensorReadout read =
+//                new SensorReadout(null,
+//                                "first",
+//                                System.currentTimeMillis(),
+//                                System.currentTimeMillis(),
+//                                0,
+//                                0,
+//                                0,
+//                                0);
+//        TextView textView = findViewById(R.id.textView);
+//        textView.setText(read.toString());
 
         //create, get, register accelerometer
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE); // get an instance of system sensor
@@ -82,12 +94,55 @@ public class SleepSessionActivity extends AppCompatActivity implements SensorEve
 
             //create a time internal
             long curTime = System.currentTimeMillis();
-            long diffTime = curTime - lastUpdate;
+            long diffTime = (curTime - lastUpdate)*1000;
             lastUpdate = curTime;
+            float speed = Math.abs(x + y + z - last_x - last_y - last_z)/ diffTime;
 
-//            TextView textView = findViewById(R.id.textView);
-//            textView.setText("x = " + x + "\n" + "y = " + y + "\n"+ "z = " + z + "\n");
-//            textView.append("Speed " + Math.abs(x + y + z - last_x - last_y - last_z)/ diffTime * 1000);
+            /*Write to file if speed is greater than threshold
+            * */
+            if (speed > SENSOR_THRESHOLD) {
+                readout = new SensorReadout(null,
+                                            "reading",
+                                            curTime,
+                                            lastUpdate,
+                                            speed,
+                                            x, y, z);
+                String string = readout.toString();
+                if (!file.exists()){
+                    try {
+                        file.createNewFile();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                try {
+                    fileOutputStream = openFileOutput(FILE_NAME, MODE_APPEND);
+                    fileOutputStream.write(string.getBytes());
+
+                    //Debug, it is writing to file
+                    Toast.makeText(this, "File written:" + FILE_NAME, Toast.LENGTH_SHORT).show();
+
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(this, "Exception: " + e.getMessage(),
+                            Toast.LENGTH_SHORT).show();
+                }finally {
+                    try {
+                        fileOutputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+
+            TextView textView = findViewById(R.id.textView);
+            textView.setText("x = " + x + "\n" + "y = " + y + "\n"+ "z = " + z + "\n");
+            textView.append("Speed " + speed);
 
             last_x = x;
             last_y = y;
@@ -100,4 +155,6 @@ public class SleepSessionActivity extends AppCompatActivity implements SensorEve
     public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
+
+
 }
