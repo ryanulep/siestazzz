d58d8d8cfb51ceb2b08e4ddc221fb8da84416451
Rick <rwboshae@gmail.com>
2018-02-14 18:29:14 -0800

    Added BellTower singleton.

---
+++ mobile/src/main/java/com/ucr/buzuka/siestazzz/BellTower.java
@@ -0,0 +1,31 @@
+package com.ucr.buzuka.siestazzz;
+
+import android.content.Context;
+
+/**
+ * Created by Rick Boshae on 2/14/2018.
+ *
+ * AlarmKeeper maintains a list of alarms created by the user.
+ *
+ * AlarmKeeper follows the Singleton data pattern. This ensures that only a single list of alarms
+ * can be accessed across.
+ */
+
+public class BellTower {
+    private static BellTower sBellTower;          // the s prefix on sBelltower is a naming convention indicating that sAlarmKeeper is static.
+
+    public static BellTower get(Context context) {
+        // if an instance of AlarmKeeper does not exist, create one.
+        if (sBellTower == null) {
+            sBellTower = new BellTower(context);
+        }
+        return sBellTower;
+    }
+
+    private BellTower(Context context) {
+        // empty
+    }
+
+
+
+}
