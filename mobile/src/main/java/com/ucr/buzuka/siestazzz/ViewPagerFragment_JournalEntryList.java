d10b2f960a855c7fc2ec10b1e29bd474dd7ca6c4
Wanderrer <rwboshae@gmail.com>
2018-02-18 18:17:17 -0800

    Added Journal Recycler View Skeleton

--- mobile/src/main/java/com/ucr/buzuka/siestazzz/ViewPagerFragment_JournalEntryList.java
+++ mobile/src/main/java/com/ucr/buzuka/siestazzz/ViewPagerFragment_JournalEntryList.java
@@ -9,12 +9,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 /**
  * Created by Rick Boshae on 1/23/2018.
  */
 
 public class ViewPagerFragment_JournalEntryList extends Fragment {
+
     private RecyclerView mJournalEntryRecyclerView;
+    private JournalEntryAdapter mAdapter;
 
     @Nullable
     @Override
@@ -27,7 +31,52 @@ public class ViewPagerFragment_JournalEntryList extends Fragment {
         mJournalEntryRecyclerView = (RecyclerView) view.findViewById(R.id.journal_recycler_view);
         mJournalEntryRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
+        updateUI();
+
         return view;
 
+    }
+
+    public void updateUI(){
+        Journal journal = Journal.get(getActivity());
+        List<JournalEntry> journalEntries = journal.getJournalEntries();
+
+        mAdapter = new JournalEntryAdapter(journalEntries);
+        mJournalEntryRecyclerView.setAdapter(mAdapter);
+    }
+
+    // View Holder
+    private class JournalEntryHolder extends RecyclerView.ViewHolder {
+        // Inflate the list item and immediately pass it to the super ViewHolders constructor.
+        public JournalEntryHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_item_joural_entry, parent, false));
+        }
+    }
+
+    // View Adapter
+    private class JournalEntryAdapter extends RecyclerView.Adapter<JournalEntryHolder> {
+
+        private List<JournalEntry> mJournalEntries;
+
+        public JournalEntryAdapter(List<JournalEntry> journalEntries) {
+            mJournalEntries = journalEntries;
+        }
+
+        @Override
+        public JournalEntryHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+
+            return new JournalEntryHolder(layoutInflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(JournalEntryHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mJournalEntries.size();
+        }
     }
 }
