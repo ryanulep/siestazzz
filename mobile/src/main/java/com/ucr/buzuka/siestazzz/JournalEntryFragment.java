bbd2ecd79e1fb66b09f4a9b2b8a4cbd818abb078
Rick <rwboshae@gmail.com>
2018-02-21 23:58:21 -0800

    Added detailed Activity for Journal Entry

---
+++ mobile/src/main/java/com/ucr/buzuka/siestazzz/JournalEntryFragment.java
@@ -0,0 +1,86 @@
+package com.ucr.buzuka.siestazzz;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.UUID;
+
+/**
+ * Created by Rick Boshae on 2/21/2018.
+ */
+
+public class JournalEntryFragment extends Fragment {
+
+    private static final String ARG_JOURNAL_ENTRY_ID = "journal_entry_id"; // Used to attach the arguments bundle to a fragment.
+
+    private JournalEntry mJournalEntry;
+    private EditText mTitleField;
+
+    /**
+     * Notes on public static JournalEntryFragment newInstance(UUID journalEntryId):
+     * =========================================================================================================
+     * To attach the arguments bundle to a fragment, you call Fragment.setArguments(Bundle). Attaching
+     * arguments to a fragment must be done after the fragment is created but before it is added to an activity.
+     *
+     * To hit this window. It is a common Android programming convection to add a static method named
+     * newInstance() to the Fragment class. This method creates the fragment instance and bundles up and
+     * sets it s arguments.
+     *
+     */
+    // TODO: Ryan, I think this is the part that is missing from your code.
+
+    // Attaching arguments to a fragment
+    public static JournalEntryFragment newInstance(UUID journalEntryId) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_JOURNAL_ENTRY_ID, journalEntryId);
+
+        JournalEntryFragment fragment = new JournalEntryFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // TODO: This part too Ryan.
+        // Retrieving arguments.
+        UUID journalEntryId = (UUID) getArguments().getSerializable(ARG_JOURNAL_ENTRY_ID);
+        mJournalEntry = Journal.get(getActivity()).getJournalEntry(journalEntryId);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_journal_detailed, container, false);
+
+        mTitleField = (EditText) view.findViewById(R.id.journal_entry_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // This space intentionally left blank.
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mJournalEntry.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // This one too.
+            }
+        });
+
+        return view;
+    }
+}
