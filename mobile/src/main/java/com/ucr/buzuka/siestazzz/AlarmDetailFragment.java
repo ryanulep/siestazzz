e9840ff50bdad41c3b8ffe72f4f4f37c4192e19e
Rick <rwboshae@gmail.com>
2018-03-07 17:52:56 -0800

    Merged from master

--- mobile/src/main/java/com/ucr/buzuka/siestazzz/AlarmDetailFragment.java
+++ mobile/src/main/java/com/ucr/buzuka/siestazzz/AlarmDetailFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
